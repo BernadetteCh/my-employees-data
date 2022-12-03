edf6102a58a4d94241cfe9681c666158ebccc738
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-03 15:38:02 +0100

    Implement function to connect to database

--- server/server.js
+++ server/server.js
@@ -4,6 +4,23 @@ const port = 8080;
 
 app.use(express.json());
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+
+const main = () => {
+  if (!process.env.MONGO_URL) {
+    console.log("Error no MongoUrl");
+    process.exit(1);
+  } else {
+    console.log(process.env.MONGO_URL);
+    console.log("Connected to database");
+    mongoose.connect(process.env.MONGO_URL);
+  }
+};
+
+main();
+
 app.get("/", (req, res) => {
   res.send("Hi from server");
 });
