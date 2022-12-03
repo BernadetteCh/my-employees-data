e1b7a48ae033420284f21a574ccf5422b8b4c470
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-03 16:49:12 +0100

    Create Navbar component

---
+++ client/my-app/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <ul className="navbar">
+      <Link to="/" className="navbar-link">
+        Home
+      </Link>
+      <Link to="/create" className="navbar-link">
+        Create
+      </Link>
+    </ul>
+  );
+};
+
+export default Navbar;
