7ea5ce6a1fd7da1a94afbec6b7c47830d3a581ee
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 09:51:24 +0100

    Create form to add new employee

--- client/my-app/src/components/Create.js
+++ client/my-app/src/components/Create.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import "../App.css";
 
 const Create = () => {
-  return <div>Hello from create</div>;
+  return (
+    <div className="form-create">
+      <h2>Create a new Employee</h2>
+      <form>
+        <label>Name:</label>
+        <input type="text"></input>
+        <label>Position:</label>
+        <input type="text"></input>
+        <label>Level:</label>
+        <input type="text"></input>
+        <Button className="submit-button">Create new Employee</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
