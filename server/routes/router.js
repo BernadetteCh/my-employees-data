cb3fc665236d0c63f0469e255edc36211cb8ff1f
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 21:07:25 +0100

    Implement put request server side

--- server/routes/router.js
+++ server/routes/router.js
@@ -25,6 +25,30 @@ router.post("/save/newemployee", async (req, res) => {
   }
 });
 
+router.put("/edit/:id", async (req, res) => {
+  const { name, position, level } = req.body.data;
+  try {
+    await Employee.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: { name: name, position: position, level: level } }
+    ).then((result) => {
+      res.status(200).json(result);
+    });
+  } catch (err) {
+    res.status(404).send();
+  }
+});
+
+router.get("/edit/:id", async (req, res) => {
+  try {
+    await Employee.findById({ _id: req.params.id }).then((result) => {
+      res.status(200).json(result);
+    });
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
 router.delete("/delete/:id", async (req, res) => {
   try {
     await Employee.findOneAndDelete({ _id: req.params.id });
