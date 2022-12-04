ff5f1e46848fcebda4c47007fe235890f84a5825
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 19:11:21 +0100

    Implement useEffect to fetch data

--- client/my-app/src/components/Edit.js
+++ client/my-app/src/components/Edit.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
-const Edit = ({ employeeId }) => {
+const Edit = () => {
   const [data, setDat] = useState([]);
+  const params = useParams();
+  const id = params.id.toString();
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch(`http://localhost:8080/api/edit/`);
-  //     const data = await response.json();
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(id);
+      const response = await fetch(`http://localhost:8080/api/edit/${id}`);
+      const data = await response.json();
 
-  //     if (!response.ok) {
-  //       console.log(`Error: ${response.status} ${response.statusText}`);
-  //     } else {
-  //       setDat(data);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
-  console.log(employeeId);
+      if (!response.ok) {
+        console.log(`Error: ${response.status} ${response.statusText}`);
+      } else {
+        setDat(data);
+      }
+    };
+    fetchData();
+  }, [params.id]);
+
   return <div>Hello from edit</div>;
 };
 
