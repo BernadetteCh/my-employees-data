df9d7d9fd2137894780f58e3e931acd7e315ae2b
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 11:19:48 +0100

    Implement useState to save and acces inputvalues

--- client/my-app/src/components/Create.js
+++ client/my-app/src/components/Create.js
@@ -8,24 +8,50 @@ const Create = () => {
     position: "",
     level: "",
   });
+
+  const upDateInputValue = (e) => {
+    setInputValue((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const saveData = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="form-create">
       <h2>Create a new Employee</h2>
       <form>
         <label className="d-block">Name:</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          name="name"
+          value={inputValue.name}
+          onChange={upDateInputValue}
+        ></input>
         <label className="d-block">Position:</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          name="position"
+          value={inputValue.position}
+          onChange={upDateInputValue}
+        ></input>
         <label className="d-block">Level:</label>
-        <input type="radio" id="intern" name="filter" value="intern" />
+        <input
+          type="radio"
+          id="intern"
+          name="level"
+          value="intern"
+          onChange={upDateInputValue}
+        />
         <label htmlFor="intern" className="me-5">
           Intern
         </label>
         <input
           type="radio"
           id="junior-developer"
-          name="filter"
+          name="level"
           value="junior-developer"
+          onChange={upDateInputValue}
         ></input>
         <label htmlFor="junior-developer" className="me-5">
           Junior Developer
@@ -33,13 +59,14 @@ const Create = () => {
         <input
           type="radio"
           id="senior-developer"
-          name="filter"
+          name="level"
           value="senior-developer"
+          onChange={upDateInputValue}
         ></input>
         <label htmlFor="senior-developer" className="me-5">
           Senior Developer
         </label>
-        <Button type="submit" className="submit-button">
+        <Button type="submit" className="submit-button" onClick={saveData}>
           Create new Employee
         </Button>
       </form>
