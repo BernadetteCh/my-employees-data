2bd2b4b2739aa9ef3de562eef0bc1665907332c5
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 15:25:56 +0100

    Add edit and delete button

--- client/my-app/src/components/TableRow.js
+++ client/my-app/src/components/TableRow.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+// import { Link } from "react-router-dom";
 
-const TableRow = ({ employee, key }) => {
+const TableRow = ({ employee, employees, myKey }) => {
+  const deleteEmployee = async (e) => {
+    const id = employee._id;
+    const response = await fetch(`http://localhost:8080/api/delete/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      console.log(`Error: ${response.status} ${response.statusText}`);
+    } else {
+      // const newList = [...employees];
+      // newList.splice(myKey, 1);
+    }
+  };
   return (
     <tr>
       <td>{employee.name}</td>
       <td>{employee.position}</td>
       <td>{employee.level}</td>
       <td>
-        <Link to="/edit">Edit</Link> | <Link to="delete">Delete</Link>
+        <Button variant="primary">Edit</Button> |{" "}
+        <Button variant="secondary" onClick={deleteEmployee}>
+          Delete
+        </Button>
       </td>
     </tr>
   );
