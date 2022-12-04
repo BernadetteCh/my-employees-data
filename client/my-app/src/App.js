86717431998c1824641fb194273f000a57ed968c
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 08:50:10 +0100

    Install bootstrap

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Create from "./components/Create";
 import Home from "./components/Home";
--- client/my-app/src/components/Home.js
+++ client/my-app/src/components/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
-  return <div>Hi fom Home</div>;
+  return (
+    <div>
+      Hi fom Home
+      <Button>Hallo Bootstrap</Button>
+    </div>
+  );
 };
 
 export default Home;
