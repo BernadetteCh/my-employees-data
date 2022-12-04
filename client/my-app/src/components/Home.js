24c20fbd20c500972e22099c091faec14993e991
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 14:08:03 +0100

    Create get request and table to display data

--- client/my-app/src/components/Home.js
+++ client/my-app/src/components/Home.js
@@ -1,11 +1,43 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import TableRow from "../components/TableRow";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/api/");
+      const data = await response.json();
+
+      if (!response.ok) {
+        console.log(`Error:${response.status} ${response.statusText}`);
+      }
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      Hi fom Home
-      <Button>Hallo Bootstrap</Button>
+      <h1 style={{ textAlign: "center" }}>My Employees Data 🙃🍀</h1>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data === []
+            ? console.log("..loading")
+            : data.map((employee) => {
+                return <TableRow employee={employee} key={employee.id} />;
+              })}
+        </tbody>
+      </Table>
     </div>
   );
 };
