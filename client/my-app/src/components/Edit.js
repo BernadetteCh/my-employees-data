521f23095c0b254918e504e457fd8ae9d3d53bbc
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 17:41:26 +0100

    Create edit component

--- client/my-app/src/components/Edit.js
+++ client/my-app/src/components/Edit.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Edit = () => {
+const Edit = ({ employeeId }) => {
+  const [data, setDat] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch(`http://localhost:8080/api/edit/`);
+  //     const data = await response.json();
+
+  //     if (!response.ok) {
+  //       console.log(`Error: ${response.status} ${response.statusText}`);
+  //     } else {
+  //       setDat(data);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
+  console.log(employeeId);
   return <div>Hello from edit</div>;
 };
 
