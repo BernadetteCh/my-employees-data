cb7deb5a0b0349521afb32b5abbbc413f423af44
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 09:13:55 +0100

    No relevant changes

--- client/my-app/src/components/Navbar.js
+++ client/my-app/src/components/Navbar.js
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <ul className="navbar">
-      <Link to="/" className="navbar-link">
-        Home
-      </Link>
+      <Link to="/" className="navbar-link"></Link>
       <Link to="/create" className="navbar-link">
         Create
       </Link>
