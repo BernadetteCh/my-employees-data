e187da810be93719ea5488158cfc48ebb980cd24
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 12:08:48 +0100

    Implement post request to save new employee

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Employee = require("../db/employeeSchema");
 
-router.get("/", (req, res) => {
-  res.send("Hi from router");
+router.post("/", async (req, res) => {
+  const { name, position, level } = req.body.inputValue;
+  const newEmployee = new Employee({
+    name: name,
+    position: position,
+    level: level,
+  });
+  try {
+    await newEmployee.save();
+    res.status(200).send();
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
