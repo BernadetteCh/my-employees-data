bfdfd115aefade097da41f4cf82b21dd9cc20cd8
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-04 09:16:54 +0100

    Include img of technology icons

--- client/my-app/src/components/Navbar.js
+++ client/my-app/src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logos from "../images/technology-logos.png";
 
 const Navbar = () => {
   return (
     <ul className="navbar">
-      <Link to="/" className="navbar-link"></Link>
+      <Link to="/" className="navbar-link">
+        <img src={logos} alt="technology-icons" className="logos"></img>
+      </Link>
       <Link to="/create" className="navbar-link">
         Create
       </Link>
