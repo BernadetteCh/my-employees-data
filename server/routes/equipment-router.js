2fa73243321cdee1a609ed01208af2bde564efc0
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-07 13:38:03 +0100

    Remove code from put request

--- server/routes/equipment-router.js
+++ server/routes/equipment-router.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { model } = require("mongoose");
+// const { model } = require("mongoose");
 const router = express.Router();
-// const Equipment = require("../db/equipmentSchema/Equipment");
-const Module = require("../db/equipmentSchema");
-const Equipment = Module.Equipment;
-const Employee = Module.Employee;
+const Equipment = require("../db/equipmentSchema");
+// const Module = require("../db/equipmentSchema");
+// const Equipment = Module.Equipment;
+// const Employee = Module.Employee;
 
 router.get("/", async (req, res) => {
   await Equipment.find()
@@ -17,24 +17,7 @@ router.get("/", async (req, res) => {
 });
 
 router.put("/assignList", async (req, res) => {
-  console.log(Employee);
-  const employee = await Employee.find({ firstName: req.body.employee }).then(
-    (result) => {
-      return result[0];
-    }
-  );
-  const equipment = await Equipment.find({
-    type: req.body.equipment.type,
-  }).then((result) => {
-    result[0];
-  });
-  //   const newS = await Equipment.updateOne(
-  //     { name: req.body.employee },
-  //     { $set: { equipment: req.body.equipment } },
-  //     { multi: true }
-  //   );
-  //   console.log(newS);
-  // console.log(req.body);
+  console.log(req.body);
 });
 
 module.exports = router;
