4371e67dac19df6bdc136ac0aaa1f80216fb214e
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-07 13:37:46 +0100

    Change Schema

--- server/db/equipmentSchema.js
+++ server/db/equipmentSchema.js
@@ -5,10 +5,9 @@ const equipmentSchema = new mongoose.Schema({
   name: String,
   type: String,
   amount: Number,
-  employee: { type: mongoose.Types.ObjectId, ref: employeeSchema },
 });
 
-const Employee = employeeSchema;
-const Equipment = mongoose.model("equipment", equipmentSchema);
-// module.exports = mongoose.model("equipment", equipmentSchema);
-module.exports = { Employee, Equipment };
+// const Employee = employeeSchema;
+// const Equipment = mongoose.model("equipment", equipmentSchema);
+module.exports = mongoose.model("equipment", equipmentSchema);
+// module.exports = { Employee, Equipment };
