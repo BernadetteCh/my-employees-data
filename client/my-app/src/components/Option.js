dfcfff5670aabda237010a5407d5f889124cdf6a
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-08 16:22:32 +0100

    Add name to option Component

--- client/my-app/src/components/Option.js
+++ client/my-app/src/components/Option.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Option = ({ value, option }) => {
-  return <option value={value}>{option}</option>;
+const Option = ({ value, option, name }) => {
+  return (
+    <option value={value} name={name}>
+      {option}
+    </option>
+  );
 };
 
 export default Option;
