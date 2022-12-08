807f2e6cdd6cbe272370bf1486b1d1ef3dcf7604
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-08 16:23:52 +0100

    Create form to edit equipment data

--- client/my-app/src/components/Equipment/EditEquipment.js
+++ client/my-app/src/components/Equipment/EditEquipment.js
@@ -4,7 +4,27 @@ import { useNavigate } from "react-router-dom";
 import Input from "../Input";
 
 const EditEquipment = () => {
-  return <div>HI from EditEquipment</div>;
+  return (
+    <div>
+      <h2>Edit an Equipment</h2>
+      <form>
+        <label className="d-block">Name</label>
+        <Input type={"text"} name="name" value={""} upDateInputValue={""} />
+        <label className="d-block">Type</label>
+        <Input type={"text"} name={"type"} value={""} upDateInputValue={""} />
+        <label className="d-block">Amount</label>
+        <Input
+          type={"number"}
+          name={"amount"}
+          value={""}
+          upDateInputValue={""}
+        />
+        {/* <Button type="submit" className="submit-button" onClick={saveData}>
+          Create new Equipment
+        </Button> */}
+      </form>
+    </div>
+  );
 };
 
 export default EditEquipment;
