ee05dda2eecc74bf4df1ab627d36f09830a56ce1
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-08 18:20:48 +0100

    Add icons to sort data , create get request to sort data

--- client/my-app/src/components/Equipment/EquipmentTable.js
+++ client/my-app/src/components/Equipment/EquipmentTable.js
@@ -1,18 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import EquipmentTableRow from "./EquipmentTableRow";
+import "../../App.css";
 
 const EquipmentTable = ({ equipmentData, updateData, employeeData }) => {
   const updateEquipmentList = (data) => {
+    updateData(data);
+  };
+  const sortEquipment = async (key, method) => {
+    const response = await fetch(
+      `http://localhost:8080/equipment/sort/${key}/${method}`
+    );
+    const data = await response.json();
     updateData(data);
   };
   return (
     <Table striped>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Type</th>
-          <th>amount</th>
+          <th>
+            Name{" "}
+            <button
+              className="sortButton"
+              onClick={() => sortEquipment("name", "ascending")}
+            >
+              ⬆
+            </button>
+            <button
+              className="sortButton"
+              onClick={() => sortEquipment("name", "descending")}
+            >
+              ⬇
+            </button>
+          </th>
+          <th>
+            Type
+            <button
+              className="sortButton"
+              onClick={() => sortEquipment("type", "ascending")}
+            >
+              ⬆
+            </button>
+            <button
+              className="sortButton"
+              onClick={() => sortEquipment("type", "descending")}
+            >
+              ⬇
+            </button>
+          </th>
+          <th>
+            Amount{" "}
+            <button
+              className="sortButton"
+              onClick={() => sortEquipment("amount", "ascending")}
+            >
+              {" "}
+              ⬆
+            </button>
+            <button
+              className="sortButton"
+              onClick={() => sortEquipment("amount", "descending")}
+            >
+              {" "}
+              ⬇
+            </button>
+          </th>
           {/* <th>Assign Employee</th> */}
         </tr>
       </thead>
