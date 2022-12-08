1799722a3d0efa3cb0fa65214ff5fe72addbef6d
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-08 15:33:00 +0100

    Create Equipment page

---
+++ client/my-app/src/components/Equipment/EditEquipment.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Input from "../Input";
+
+const EditEquipment = () => {
+  return <div>HI from EditEquipment</div>;
+};
+
+export default EditEquipment;
