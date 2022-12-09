12238751b8e60e3083ff7e580ecbb9f965b3d92e
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-09 22:15:38 +0100

    Implement fetch to get data and return list

--- client/my-app/src/components/EquipmentEmployeeList.js
+++ client/my-app/src/components/EquipmentEmployeeList.js
@@ -1,7 +1,39 @@
 import React, { useState, useEffect } from "react";
+import "../App.css";
+
+const fetchEmployeeEquipmentList = async (dataSetter) => {
+  const url = `http://localhost:8080/api`;
+  const response = await fetch(`${url}`);
+  const data = await response.json();
+  console.log(data);
+  dataSetter(data);
+};
 
 const EquipmentEmployeeList = () => {
-  return <div>Hi from List</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetchEmployeeEquipmentList(setData);
+  }, []);
+  return (
+    <div>
+      {data === [] ? (
+        console.log("HI")
+      ) : (
+        <ul>
+          {data.map((data) => {
+            return (
+              <li key={data._id}>
+                {data.firstName} <strong>Equipment:</strong>
+                <span>{data.equipment.name}</span>
+                <strong>Amount:</strong>
+                <span>{data.equipment.amount}</span>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default EquipmentEmployeeList;
