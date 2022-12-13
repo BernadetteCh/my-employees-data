09dd5591539279bf235c3aa3f809b7d392ef2eec
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-13 21:26:27 +0100

    Include EquipmentSchema

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Employee = require("../db/employeeSchema");
+const Equipment = require("../db/equipmentSchema");
 
 router.get("/years-of-experience/:years", async (req, res) => {});
 
