78c130b189406d0eae8901247ad1055bc87cdf41
Bernadette Christiner <b.christiner93@gmail.com>
2022-12-14 11:10:15 +0100

    Create new component to sort

---
+++ client/my-app/src/components/Employee/EmployeeTableHead.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+const EmployeeTableHead = ({ sortList }) => {
+  const sortEmployee = (key, method) => {
+    sortList(key, method);
+  };
+  return (
+    <thead>
+      <tr>
+        <th>
+          FirstName
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("firstName", "ascending")}
+          >
+            ⬆
+          </button>
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("firstName", "descending")}
+          >
+            ⬇
+          </button>
+        </th>
+        <th>
+          SecondName
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("secondName", "ascending")}
+          >
+            ⬆
+          </button>
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("secondName", "descending")}
+          >
+            ⬇
+          </button>
+        </th>
+        <th>
+          LastName
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("lastName", "ascending")}
+          >
+            ⬆
+          </button>
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("lastName", "descending")}
+          >
+            ⬇
+          </button>
+        </th>
+        <th>
+          Position
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("position", "ascending")}
+          >
+            {" "}
+            ⬆
+          </button>
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("position", "descending")}
+          >
+            ⬇
+          </button>
+        </th>
+        <th>
+          Level
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("level", "ascending")}
+          >
+            ⬆
+          </button>
+          <button
+            className="sortButton"
+            onClick={() => sortEmployee("level", "descending")}
+          >
+            ⬇
+          </button>
+        </th>
+        <th>Action</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default EmployeeTableHead;
