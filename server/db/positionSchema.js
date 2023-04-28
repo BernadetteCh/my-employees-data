f5b293d05693447945273463aadf7e6947287e65
Bernadette Christiner <b.christiner93@gmail.com>
2023-04-28 21:37:23 +0200

    remove tasks from pa

--- server/db/positionSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const positionSchema = new mongoose.Schema({
-  name: String,
-  salary: String,
-  overbudget: String,
-});
-
-module.exports = mongoose.model("positions", positionSchema);
